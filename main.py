cf8f7c1a621c29360fbd73923d24a9e44341b89c
bhagat-abhishek <inst.abhishek@gmail.com>
2024-03-01 18:06:00 +0530

    fixed the folder name as per title and image download issue

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+import re
 
-url = requests.get("https://overlandingcamping.com/listing/sankri-sanctuary-haven-your-gateway-to-himalayan-overlanding-and-camping-trek-start-point-for-rupin-pass-har-ki-doon-kedarkantha/", headers={
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
-})
+url = requests.get(
+    "https://overlandingcamping.com/listing/sankri-sanctuary-haven-your-gateway-to-himalayan-overlanding-and-camping-trek-start-point-for-rupin-pass-har-ki-doon-kedarkantha/",
+    headers={
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0",
+    },
+)
 
 soup = BeautifulSoup(url.text, "html.parser")
 
@@ -13,37 +17,49 @@ title = soup.h1.get_text()
 
 # Listing Images
 images = []
-for link in soup.find_all('a', {'data-background-image': True}):
+for link in soup.find_all("a", {"data-background-image": True}):
     images.append(link["data-background-image"])
 
 # Listing Address
-address = soup.find('a', class_='listing-address').get_text(strip=True)
+address = soup.find("a", class_="listing-address").get_text(strip=True)
 
 # Listing Lat/Long
-latitude = soup.find('div', id='singleListingMap')['data-latitude']
-longitude = soup.find('div', id='singleListingMap')['data-longitude']
+latitude = soup.find("div", id="singleListingMap")["data-latitude"]
+longitude = soup.find("div", id="singleListingMap")["data-longitude"]
 
-#to get all the description
-description = soup.find('div', {'id': 'listing-overview'})
-description_text ="\n".join([p.get_text(strip=True) for p in description.find_all('p')])
-
-
-#Author and link
-    
-author_name = soup.find('div', class_='hosted-by-title').find('a').text.strip()
-author_link = soup.find('div', class_='hosted-by-title').find('a')['href']
+# to get all the description
+description = soup.find("div", {"id": "listing-overview"})
+description_text = "\n".join(
+    [p.get_text(strip=True) for p in description.find_all("p")]
+)
 
 
+# Author and link
 
-#making folder
+author_name = soup.find("div", class_="hosted-by-title").find("a").text.strip()
+author_link = soup.find("div", class_="hosted-by-title").find("a")["href"]
 
-folder_name = "overlandng"
+
+# formating the title to be used as folder name
+def convert_to_folder_name(title):
+    # Remove special characters and spaces
+    folder_name = re.sub(r"[^\w\s]", "", title)
+    # Replace spaces with underscores
+    folder_name = re.sub(r"\s+", "_", folder_name)
+    # Convert to lowercase
+    folder_name = folder_name.lower()
+    return folder_name
+
+
+# making folder
+
+folder_name = convert_to_folder_name(title)
 txt_folder = os.path.join(folder_name, "Text-folder")
 img_folder = os.path.join(folder_name, "images-folder")
 os.makedirs(txt_folder, exist_ok=True)
 os.makedirs(img_folder, exist_ok=True)
 
-#Save text and image
+# Save text and image
 
 file_name = "listing.txt"
 file_path = os.path.join(txt_folder, file_name)
@@ -58,11 +74,16 @@ with open(file_path, "w") as file:
         file.write(f"Author Name: {author_name}\n")
         file.write(f"Author Link: {author_link}\n")
 
-# downlopad and save images 
+# downlopad and save images
 
 for i, url in enumerate(images):
     try:
-        response = requests.get(url)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+
+        response = requests.get(url, headers=headers)
+
         if response.status_code == 200:
             with open(os.path.join(img_folder, f"image_{i+1}.jpg"), "wb") as f:
                 f.write(response.content)
@@ -72,6 +93,5 @@ for i, url in enumerate(images):
     except Exception as e:
         print(f"Error downloading image {i+1}: {e}")
 
+
 print("All images downloaded and saved successfully!")
-
-
